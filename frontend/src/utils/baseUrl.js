b16d3879074f6e1f24f36a75180ed6c29d493980
pranavrana <pranavrana1258@gmail.com>
2025-10-06 01:13:01 -0400

    fix issue

--- frontend/src/utils/baseUrl.js
+++ frontend/src/utils/baseUrl.js
@@ -1,6 +1,6 @@
 const getBaseUrl = () => {
     // return "http://localhost:5001"
-    return "https://bookstore-app-5ydh.vercel.app/api/books";
+    return "https://bookstore-app-5ydh.vercel.app";
 }
 
 export default getBaseUrl;
