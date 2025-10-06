845f8cf4b5411fa6b4bf1cdf2f0fcd5097588118
pranavrana <pranavrana1258@gmail.com>
2025-10-06 01:09:20 -0400

    added vecel urls

--- frontend/src/utils/baseUrl.js
+++ frontend/src/utils/baseUrl.js
@@ -1,5 +1,6 @@
 const getBaseUrl = () => {
-    return "http://localhost:5001"
+    // return "http://localhost:5001"
+    return "https://bookstore-app-5ydh.vercel.app/api/books";
 }
 
 export default getBaseUrl;
